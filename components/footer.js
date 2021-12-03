8e09add84493004e411b3ff54d9e81d19a146142
AzsenSeries <thanapatnes@gmail.com>
2021-12-03 15:57:37 +0700

    nn

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,11 @@
 
 export default function Footer() {
     return (
-        <div className="">
-             <footer className="fixed h-[210px] flex items-center justify-center w-full h-24 border-t bg-yellow-100 ">
+        <footer className="h-[210px] flex items-center justify-center w-full h-24 border-t bg-yellow-100">
              <div className="text-green-terminal text-base font-bold mb-2">
                 Copyright Ⓒ 2022 : Mee Poong Group
             </div>
         </footer>
-        </div>
-       
        
     );
   }
