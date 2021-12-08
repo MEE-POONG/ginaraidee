44d7aee7052feb8cacb42bfd1e281422ed938256
Jame2455 <93962729+Jame2455@users.noreply.github.com>
2021-12-08 09:56:28 +0700

    newj

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,10 @@
 
 export default function Footer() {
     return (
-        <footer className="h-[210px] flex items-center justify-center w-full h-24 border-t bg-yellow-100">
-             <div className="text-green-terminal text-base font-bold mb-2">
+        <footer className="h-[210px] flex items-center justify-center w-full h-24 border-t bg-yellow-100 ">
+             <div className="text-green-terminal text-base font-bold ">
                 Copyright Ⓒ 2022 : Mee Poong Group
             </div>
         </footer>
-       
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Navbar from "../components/Navbar";
 import Yourmenu from "../components/yourmenu";
-import Footer from "../components/footer"
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Yourmenu />
-      <Footer/>
-
-    </div>
+    <>
+      <div>
+        <Navbar />
+        <Yourmenu />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 }
