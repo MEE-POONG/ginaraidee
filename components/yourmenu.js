daa298cc6a594978a73342068a2827eeb13e1560
bas123s <93962379+bas123s@users.noreply.github.com>
2022-01-06 16:07:45 +0700

    +++

--- components/yourmenu.js
+++ components/yourmenu.js
@@ -4,26 +4,40 @@ import Modalstore from "./Modalstore";
 
 export default function Yourmenu() {
   return (
+    <div>
     <div className="container mx-auto max-w-lg py-5">
       <div className="px-4 flex-auto">
         <div className="text-center">
-          <h1 className="text-3xl bg-shadow font-bold py-4">วันนี้กินอะไรดี</h1>
-          <div className=" text-center pl-24">
-            <p className="text-xl italic">By MeePoong</p>
+          <h1 className="text-4xl bg-shadow font-bold py-4 px-2 ">วันนี้กินอะไรดี</h1>
+          <div className=" text-center pl-32">
+            <p className="text-2xl italic text-yellow-400">By MeePoong</p>
           </div>
         </div>
       </div>
-      <div className="  text-center place-content-center">
-        <div className="grid grid-cols-1">
-          <div> <Modalfood /></div>
-        </div>
-        <div className="grid grid-cols-1">
-          <div> <Modaldrink /></div>
-        </div>
-        <div className="grid grid-cols-1">
-          <div> <Modalstore /></div>
-        </div>
-      </div>
+      
     </div>
+    <div className="  text-center place-content-center">
+    <div className="grid lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 gap-12 ml-4 mr-4 mb-12">
+    <div>
+    <span><Modalfood /></span>
+    <span className="text-2xl "> สุ่มอาหาร  </span>
+    </div>
+   
+    <div>
+    <span>    <Modaldrink /></span>
+    <span className="text-2xl ">สุ่มเครื่องดื่ม  </span>
+
+    </div>
+
+  
+      <div>
+      <span>     <Modalstore /></span>
+      <span className="text-2xl "> สุ่มร้านค้า  </span>
+ 
+      </div>
+   
+    </div>
+  </div>
+  </div>
   );
 }
