0706dff665847310ed2653c2d99a5e6ecd5b6fd6
Jame2455 <93962729+Jame2455@users.noreply.github.com>
2022-01-07 10:37:15 +0700

    g

--- components/listbistro.js
+++ components/listbistro.js
@@ -42,16 +42,19 @@ export default function Listbistro({ data, listStore }) {
         </h1>
         <h1 className="text-2xl bg-shadow font-bold py-5">เมนูทั้งหมด</h1>
       </div>
-      {menuByStoreList?.map(({ _id, name, price }) => (
+      {menuByStoreList?.map(({ _id, name, price,img }) => (
         <div key={_id} className="flex items-center justify-center px-3 py-3">
-          <div className="max-w-md bg-white rounded-xl overflow-hidden cursor-pointer shadow-lg hover:shadow-2xl hover:scale-105 transform transition-all duration-500 w-full h-48">
+          <div className="max-w-md bg-white rounded-xl overflow-hidden cursor-pointer shadow-lg hover:shadow-2xl hover:scale-105 transform transition-all duration-500 w-full">
             <div className="p-3 text-center">
-              <Image
-                src="/images/delivery.png"
-                alt=""
-                width={100}
-                height={90}
-              />
+            <Image
+              src={
+                `http://upload-image.gin-a-rai-dee.daddybody.company/` +
+                img
+              }
+              alt=""
+              width={200}
+              height={200}
+            />
             </div>
 
             <h1 className="text-center text-lg text-gray-900 font-bold">
