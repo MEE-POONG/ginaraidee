e19bb2185d9ea5029d6271580d343653c4df2b16
AzsenSeries <thanapatnes@gmail.com>
2022-01-08 11:21:09 +0700

    1

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,14 @@
 import { BsFacebook } from "react-icons/bs";
 
 export default function Footer() {
-    return (
-        <footer className="flex items-center justify-center w-full h-24 border-t bg-yellow-100">
-             <div className="text-green-terminal text-base font-bold mb-2">
-                Copyright Ⓒ 2022 : Mee Poong Group
-            </div>
-        </footer>
-    );
-  }
+  return (
+    <div className="fixed left-0 right-0 bottom-0 bg-white p-5 bg-gray-500">
+      <div>
+        <strong className="font-bold">La protection de votre vie privée est importante</strong>
+        <p className="text-sm">Nous utilisons des cookies et des technologies similaires pour que votre visite de notre site web soit la plus agréable possible.
+          <a className="underline" href="" target="_blank">Privacy Policy</a>
+        </p>
+      </div>
+    </div>
+  );
+}
