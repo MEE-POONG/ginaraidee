5674aea14f6c42198c161177e507699be7d72fe6
Jame2455 <93962729+Jame2455@users.noreply.github.com>
2022-01-28 15:47:48 +0700

    ff

--- components/listdrinkstore.js
+++ components/listdrinkstore.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-export default function Listdrinkstore({ data, listStoredrink }) {
+export default function Listdrinkstore({listStoredrink }) {
   const [drinkByStoredrinkList, setDrinkByStoredrinkList] = useState();
 
   useEffect(() => {
